166af0b0132d3b1ba31f9229ec6094a264b1bd13
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2026-07-11 13:02:23 +0530

    Update chayan.sh

--- Build Infrastructure with Terraform on Google Cloud: Challenge Lab/chayan.sh
+++ Build Infrastructure with Terraform on Google Cloud: Challenge Lab/chayan.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+
+# Define color variables
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+
+# Define text formatting variables
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+
+clear
 echo -e "${GREEN_TEXT}${BOLD_TEXT}Enter the BUCKET name: ${RESET_FORMAT}\c"
 read BUCKET
 export BUCKET
@@ -407,3 +427,13 @@ terraform init
 terraform plan
 
 terraform apply -auto-approve
+
+# Final message
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}              LAB COMPLETED SUCCESSFULLY!              ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
+echo
+echo "${RED_TEXT}${BOLD_TEXT}${UNDERLINE_TEXT}https://www.youtube.com/@TechCode9${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}Don't forget to Like, Share and Subscribe for more Videos${RESET_FORMAT}"
+echo
